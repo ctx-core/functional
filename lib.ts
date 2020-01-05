f9caad62dea860fe9121a35af3c8816dd120015e
Brian Takita <brian.takita@gmail.com>
2020-01-05 05:34:22 -0500

    BREAKING CHANGE: _curry instead of _fn__curry
    fix: curry,flip: if terminal functional call has additional arguments, ignore the rest

--- lib.ts
+++ lib.ts
@@ -3,53 +3,59 @@
  * @see {@link http://jrsinclair.com/articles/2016/marvellously-mysterious-javascript-maybe-monad/}
  */
 export const curry =
-	_fn__curry((local, args) => Array.prototype.push.apply(local, args))
+	_curry(
+		(local, arg_a1)=>
+			Array.prototype.push.apply(local, arg_a1))
 export const flip =
-	_fn__curry((local, args) => Array.prototype.unshift.apply(local, args))
+	_curry(
+		(local, arg_a1)=>
+			Array.prototype.unshift.apply(local, arg_a1))
 export const curry__flip = flip
 /**
  *
- * @param fn__append
+ * @param append
  * @returns {function(*=): *}
  * @private
  * @see {@link https://medium.com/@kevincennis/currying-in-javascript-c66080543528}
  */
-export function _fn__curry(fn__append) {
-	return fn => {
+export function _curry(append) {
+	return fn=>{
 		const arity = fn.length
-		return (function resolver(...arg_a1__resolver:any[]) {
+		return resolver
+		function resolver(...arg_a1__resolver:any[]) {
 			const memory = Array.prototype.slice.call(arg_a1__resolver)
 			return function (...arg_a1:any[]) {
 				const local = memory.slice()
-				fn__append(local, arg_a1)
+				const remaining = Math.max(arity - local.length, 0)
+				append(local, arg_a1.slice(0, remaining))
 				const next = local.length >= arity ? fn : resolver
-				return next.apply(null, local)
+				return next(...local)
 			}
-		}())
+		}
 	}
 }
 /**
  * map :: Monad m => (a -> b) -> m a -> m b
  */
-export const map = curry((fn, m) => {
+export const map = curry((fn, m)=>{
 	return m.map(fn)
 })
 /**
  * chain :: Monad m => (a -> m b) -> m a -> m b
  */
-export const chain = curry((fn, m) => {
+export const chain = curry((fn, m)=>{
 	return m.chain(fn)
 })
 /**
  * ap :: Monad m => m (a -> b) -> m a -> m b
  */
-export const ap = curry((mf, m) => { // mf, not fn, because this is a wrapped function
+export const ap = curry((mf, m)=>{ // mf, not fn, because this is a wrapped function
 	return mf.ap(m)
 })
 /**
  * orElse :: Monad m => m a -> a -> m a
  */
-export const orElse = curry((val, m) => {
+export const orElse = curry((val, m)=>{
 	return m.orElse(val)
 })
 /**
@@ -59,6 +65,6 @@ export const orElse = curry((val, m) => {
  * @param {module:ctx-core/functional/lib~maybe} m2 - applied `maybe`
  * @returns {module:ctx-core/functional/lib~maybe}
  */
-export const liftA2 = curry((fn, m1, m2) => {
+export const liftA2 = curry((fn, m1, m2)=>{
 	return m1.map(fn).ap(m2)
 })
